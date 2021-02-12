0651c3b35b0b151feb11ab20d40a73449aeca773
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-12 23:09:31 +0500

    add these files we used dammy api jsons holfder

---
+++ index2.js
@@ -0,0 +1,124 @@
+// console.log("hello World");
+
+
+// async function asyncs(){
+//     const api=await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//    console.log(api)
+//    const json=await api.json()
+//    console.log(json)
+// }
+// asyncs()
+// console.log("hello End");
+
+
+// async function postmethod(){
+//     const post=await fetch("https://jsonplaceholder.typicode.com/posts" ,{
+//         method: "Post",
+//         body: JSON.stringify({
+//             name: "Abdul moiz",
+//             fatherName: "Zafar ali",
+//             course: "Cloud Native Computing",
+//             userId: 1,
+//         }),
+//         headers:{
+//          'content-type' : 'application/json; charset=UTF-8'
+//         }
+//     })
+    
+// console.log(post);
+// const json=await  post.json()
+// console.log(json);
+// }
+// postmethod()
+
+
+// console.log("hello World");
+
+
+// const getMethod=async () =>{
+//     const get= await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     console.log(get);
+//     const json=await get.json()
+//     console.log(json);
+// }
+// getMethod()
+
+//post method
+
+// const postMethod= async () =>{
+//     const post =await fetch("https://jsonplaceholder.typicode.com/posts" ,{
+//         method: "Post",
+//         body: JSON.stringify({
+//             fullName: "Abdul Moiz",
+//             fatherName: "Zafar ali",
+//             course: "Cloud Native Computing",
+//             rollNumber: 1
+//         }),
+//         headers:{
+//             'content-type' : 'application/json; charset=UTF-8'
+//         }
+//     })
+//     console.log(post)
+//     const jsons= await post.json()
+//     console.log(jsons)
+// }
+// postMethod()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
